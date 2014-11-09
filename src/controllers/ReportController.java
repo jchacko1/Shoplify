10598978b6e48c9b41edbf033845b14b740130eb
Kelly An <4ever.kellyan@gmail.com>
2014-11-09 12:58:15 -0500

    bug fixed

--- src/controllers/ReportController.java
+++ src/controllers/ReportController.java
@@ -2,18 +2,23 @@ package controllers;
 
 import DataAccess.ReportRepository;
 import DataAccess.ReportService;
-import bussinessLogic.ReportManager;
-import models.ReportModel;
+import businessLogic.ReportManager;
+import models.*;
+import global.Global;
 
 /**
  * Created by Kelly An on 11/5/2014
  */
-public class ReportConroller{
+public class ReportController{
 
     private static ReportManager _reportManager = new ReportManager();
 
     public static ReportModel getReportWithLogin(String reportName, String reportId)
     {
         return _reportManager.getReportWithLogin(reportName, reportId);
+    }
+
+    public static void createReport(String reportId, String reportName) {
+
     }
 }
