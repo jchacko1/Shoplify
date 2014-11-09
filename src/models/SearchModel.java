620da711149a5fa49cf39c8b4ecbc094a6934bbd
Kelly An <4ever.kellyan@gmail.com>
2014-11-09 17:32:48 -0500

    Search model related code

---
+++ src/models/SearchModel.java
@@ -0,0 +1,16 @@
+package models;
+
+import java.util.ArrayList;
+
+/**
+ * Created by KellyAnn on 11/9/14.
+ */
+public class SearchModel {
+
+    private ArrayList<ItemModel> _items;
+
+
+    public SearchModel() {
+        _items = new ArrayList<ItemModel>();
+    }
+}
